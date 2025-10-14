c0141381aab2edc28246113e1aa719ad759dd40b
Nonault <adouyere@hotmail.com>
2025-10-14 20:00:41 +0200

    init project

---
+++ src/test/java/com/vaudoise/VaudoiseInsuranceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaudoise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaudoiseInsuranceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
